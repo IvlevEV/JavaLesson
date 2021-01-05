c378933c7d15932806912884025c1db8012097cf
IvlevEV <iLynch47@gmail.com>
2021-01-05 17:37:35 +0600

    Урок 8. Написание приложения с графическим интерфейсом
    Калькулятор

--- src/Lesson08/Main.java
+++ src/Lesson08/Main.java
@@ -4,7 +4,7 @@ import javax.script.ScriptException;
 
 public class Main {
     public static void main(String[] args) throws ScriptException {
-       // Calculator calculator = new Calculator();
-        ScriptEngine scriptEngine = new ScriptEngine();
+       Calculator calculator = new Calculator();
+        //ScriptEngine scriptEngine = new ScriptEngine();
     }
 }
